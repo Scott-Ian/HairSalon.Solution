be1df1ac81641ef5c9a2cdf34aa40c31d6cedd21
Ian Scott <chesnekov@gmail.com>
2020-07-31 10:05:03 -0700

    adds Index route and db reference to StylistsController.cs

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controllers
+  {
+    private readonly HairSalonContext _db;
+    public HairSalonContext (HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> stylists = _db.Stylists.ToList();
+      return View(stylists);
+    }
+  }
+}
